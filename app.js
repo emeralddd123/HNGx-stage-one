754d8deb6cc6d885cab9e61537985a3325c87864
Usman Abdulsalam <usmanabdulsalamolanrewaju@gmail.com>
2023-09-07 20:54:01 +0100

    done!!

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ app.get("/", (req, res) => {
 
     let date = new Date()
 
-    res.json({
+    res.status(200).json({
         "slack_name":`${slack_name}`,
         "current_day":"Thursday",
         "utc_time":`${date}`,
         "track":`${track}`,
-        "github_file_url":"",
-        "github_repo_url":"",
+        "github_file_url":"https://github.com/emeralddd123/HNGx-stage-one/blob/main/app.js",
+        "github_repo_url":"https://github.com/emeralddd123/HNGx-stage-one",
         "status_code":200
     })
 });
